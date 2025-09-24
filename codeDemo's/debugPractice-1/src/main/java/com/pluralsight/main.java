b1b4754d7e82f1a2384d3d8b8c4d044b9421cec4
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-24 17:24:41 -0400

    Debugging practice 1

---
+++ codeDemo's/debugPractice-1/src/main/java/com/pluralsight/main.java
@@ -0,0 +1,18 @@
+// Challenge: Debug This Code
+// public class Test
+// public static void main(String[] args) {
+// int age = "twenty";// Error!
+// boolean happy = "yes";// Error
+// System.out.println("Age:" + age)
+// System.out.println("Happy:" + happy)
+
+package com.pluralsight;
+
+public class main {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean happy = true;
+        System.out.println("Age:" + age );
+        System.out.println("Happy:" + happy);
+    }
+}
