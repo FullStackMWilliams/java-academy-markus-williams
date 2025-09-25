a4dc993285a705304d875101e7d7e46002873b6b
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-24 23:54:11 -0400

    StringApplications project

--- workbook1/StringApplication/src/main/java/com/pluralsight/VariableApp.java
+++ workbook1/StringApplication/src/main/java/com/pluralsight/VariableApp.java
@@ -1,6 +1,47 @@
+package com.pluralsight;
+
 public class VariableApp {
     public static void main(String[] args) {
-        // Question 1:
-        // declare
+
+ //                          Exercises - Operators
+
+ //            Step 1
+
+ // Write code to find answers to the questions.
+
+ // Questions:
+
+        // 1. Declare a variable to store your favorite color
+        String favColor = "Purple";
+           System.out.println(favColor);
+
+        // 2. Declare a variable to store the year you started this class.
+        int startYear = 2025;
+        System.out.println(startYear);
+
+        // 3. Declare a variable to store your middle initial.
+        char middleInitial = 'H';
+        System.out.println(middleInitial);
+
+        // 4. Declare a variable to store whether you have pets. (yes/no)?
+        String ownsPet = "No";
+        System.out.println(ownsPet);
+
+        // 5. Declare a variable with a nice message or saying
+        String greetings = "Instructor Remsey rocks";
+        System.out.println(greetings);
+
+        // 6. Print each variable with a descriptive message.
+        System.out.println("My favorite color is " + favColor + ".");
+        System.out.println("I started Year Up in " + startYear + ".");
+        System.out.println("My middle name starts with a " + middleInitial + ".");
+        System.out.println("Do you own pets? " + ownsPet + ".");
+
+
+
+
+
+
+
     }
 }
