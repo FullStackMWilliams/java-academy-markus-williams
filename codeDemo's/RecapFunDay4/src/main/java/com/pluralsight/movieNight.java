73779f61bee64b0e958c86a2d22149a4c51bc8cc
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-25 11:44:44 -0400

    Added recap fun day exercises to codeDemo's folder.

---
+++ codeDemo's/RecapFunDay4/src/main/java/com/pluralsight/movieNight.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class movieNight {
+    public static void main(String[] args) {
+String movieTitle = "John Wick 3";
+int numOfFriends = 4;
+double pizzaSlice_perPrice = 2.99;
+boolean hasPopcorn = true;
+
+        }
+    }
+}
