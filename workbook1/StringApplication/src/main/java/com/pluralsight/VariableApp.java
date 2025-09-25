d9df38b2923ed80f396b0b2b3b75d24604e91fe2
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-25 00:16:23 -0400

    StringApplications project. Step 2 complete

--- workbook1/StringApplication/src/main/java/com/pluralsight/VariableApp.java
+++ workbook1/StringApplication/src/main/java/com/pluralsight/VariableApp.java
@@ -7,7 +7,7 @@ public class VariableApp {
 
  //            Step 1
 
- // Write code to find answers to the questions.
+ // Write code to find answers to the following questions.
 
  // Questions:
 
@@ -28,7 +28,7 @@ public class VariableApp {
         System.out.println(ownsPet);
 
         // 5. Declare a variable with a nice message or saying
-        String greetings = "Instructor Remsey rocks";
+        String greetings = "Instructor Remsey rocks!";
         System.out.println(greetings);
 
         // 6. Print each variable with a descriptive message.
@@ -36,6 +36,30 @@ public class VariableApp {
         System.out.println("I started Year Up in " + startYear + ".");
         System.out.println("My middle name starts with a " + middleInitial + ".");
         System.out.println("Do you own pets? " + ownsPet + ".");
+
+//             Step 2
+
+//  Declare AND initialize variables for:
+
+        // 1. The number of days in a week
+        short oneWeek = 7;
+
+        // 2. The price of coffee
+        float coffee = 4.99F;
+
+        // 3. Your favorite letter
+        char favLetter = 'M';
+
+        // 4. Whether it's raining or not
+        boolean isRaining = true;
+
+        // 5. Print all values in complete sentences
+        System.out.println("One week is equal to " + oneWeek + " days.");
+        System.out.println("That'll be " + "$" + coffee + ", sir.");
+        System.out.println("My favorite letter of course is " + favLetter + "!");
+        System.out.println("It's " + isRaining + " that it's raining.");
+
+
 
 
 
