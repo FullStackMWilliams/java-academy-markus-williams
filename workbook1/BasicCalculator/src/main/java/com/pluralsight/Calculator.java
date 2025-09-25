18a2bfd8de2381e74668d7df7562ab7c4c1564d8
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-25 15:15:39 -0400

    In progress of making my Calculator app

---
+++ workbook1/BasicCalculator/src/main/java/com/pluralsight/Calculator.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+//                  Exercise 3
+
+// Notes: Create a Java Application named BasicCalculator that reads in two floating point numbers and then asks the user whether they want to add, subtract, multiply, or divide the two numbers.
+// Notes: For this exercise, only code the questions. We won't calculate add, subtract, multiply, or divide.
+// For this exercise, always multiply.
+
+        Scanner readMeBot = new Scanner(System.in);
+        System.out.print( );
+
+//  Step 1
+
+        // 1.) Perform the requested operation and display the results
+
+        // Enter the first number: 5
+        // Enter the second number: 12
+        // Select multiply
+
+
+
+
+
+    }
+}
