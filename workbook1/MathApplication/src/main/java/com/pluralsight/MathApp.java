e1a1e75c05382879cc71892ac7cb1f3b8e29d073
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-25 00:57:59 -0400

    Math application added, question 1 completed.

---
+++ workbook1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+//                             Exercise 3
+
+//           Step 1
+
+    // Write code to find answers to the following questions
+
+    // NOTES: Coding is not just about solving equations, your code should be legible and meaningful.
+    // Use comment and line spacing to visually separate each question.
+
+//  Questions:
+
+        // 1.) Create two variables to represent the salary for Bob and Gray, name them  bobSalary and graySalary.
+         // Create a new variable named highestSalary.
+         // Determine whose salary is greater using Math.max() and store the answer in highestSalary.
+         // Set the initial salary variables to any value you want.
+         // Print the answer (i.e " The highest salary is...)
+
+        double bobSalary = 46000.00;
+        double graySalary = 53000.00;
+        double highestSalary = Math.max(graySalary, bobSalary);
+        System.out.println(highestSalary);
+
+       // 2.) Find and display
+
+
+    }
+    }
