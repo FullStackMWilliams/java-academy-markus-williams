99af3821e2f7193c181f124611d9a84c919e4b85
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-26 19:49:27 -0400

    revised and added file on how to debug IntelliJ

---
+++ workbook1/SandwichShop-Application/src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+
+        double regSized_Sandwich = 5.45;
+        double LrgSized_Sandwich = 8.95;
+
+        Scanner myCashier = new Scanner(System.in);
+
+
+
+        System.out.println("Do you want to make your sandwich a large? ")
+
+
+
+
+
+        }
+    }
+
+    public static void
