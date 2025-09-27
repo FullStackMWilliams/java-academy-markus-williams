cdc49148a368d875baacd2cf57d44af7e1c3c8b6
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-26 20:38:35 -0400

    Half way done with SandwichShop

--- workbook1/SandwichShop-Application/src/main/java/com/pluralsight/SandwichShop.java
+++ workbook1/SandwichShop-Application/src/main/java/com/pluralsight/SandwichShop.java
@@ -4,21 +4,38 @@ import java.util.Scanner;
 
 public class SandwichShop {
     public static void main(String[] args) {
-
-        double regSized_Sandwich = 5.45;
-        double LrgSized_Sandwich = 8.95;
-
         Scanner myCashier = new Scanner(System.in);
 
-
-
-        System.out.println("Do you want to make your sandwich a large? ")
-
-
+//                          Exercise: Sandwich Shop
+// Notes: This will be a point of sales application to calculate the cost of a sandwich.
 
 
 
-        }
+// Step 1:
+// Prompt hte user for the size of the sandwich (1 or 2):
+
+        // a.) 1: Regular: base price $5.45
+        System.out.println("What would you like to order?");
+        System.out.println("1: A regular sandwich is $5.45");
+
+        // b.) 2: Large: base price $8.95
+        System.out.println("2: A large sandwich is $8.95 ");
+
+       // line below checks the line above
+        System.out.println("What would you like: 1 or 2");
+        double customerWants = myCashier.nextDouble();
+
+       // This line acts as a placeholder
+        double customerPrice = 0.0;
+
+        if (customerWants == 1) {
+            customerPrice = 5.45;
+        } else if (customerWants == 2) {
+            customerPrice = 8.89;
+        } else
+            System.out.println("Enter 1 or 2 to receive price.");
+
+        System.out.println(customerPrice);
     }
+}
 
-    public static void
