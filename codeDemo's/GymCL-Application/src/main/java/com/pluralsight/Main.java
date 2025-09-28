0df79f1be92c394be5a39ee0ab4ee58991c1b992
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-28 15:12:41 -0400

    starting to get the logic.. Part 2 almost finished T-T

--- codeDemo's/GymCL-Application/src/main/java/com/pluralsight/Main.java
+++ codeDemo's/GymCL-Application/src/main/java/com/pluralsight/Main.java
@@ -23,13 +23,13 @@ public class Main {
         String wantsMembership = gymClerk.next();
 
         System.out.println("Would you like to sign-up for our Bronze package for $35? (yes/no): ");
-        String bronzePackage = gymClerk.next();
+        String wantsBronzePackage = gymClerk.next();
 
         System.out.println("would you like to sign up for our Silver package for $55? (yes/no): ");
-        String silverPackage = gymClerk.next();
+        String wantsSilverPackage = gymClerk.next();
 
         System.out.println("Would you like to sign-up for our Gold package for $85? (yes/no): ");
-        String goldPackage = gymClerk.next();
+        String wantsGoldPackage = gymClerk.next();
 
         // b.) Membership start date (Ex. (9/29/30)):
         System.out.println("Enter today's date (Ex. 9/17/2025): ");
@@ -68,23 +68,52 @@ public class Main {
         // c.) Gold membership price:
         double goldMembership_Price = 85.00;
 
+        double membershipBase_Price = 0.00;
+
+        if (wantsMembership.equals("yes")) {
+            gymClerk.next();
+        }
+        if (wantsBronzePackage.equals("yes")){
+            membershipBase_Price = 35.00;
+        }if (wantsSilverPackage.equals("yes")) {
+            membershipBase_Price = 55.00;
+        }if (wantsGoldPackage.equals("yes")){
+            membershipBase_Price = 85.00;
+        }
+
         // d.) Guest pass:
         double guestPass_Price = 10.00;
+        if (wantsGuestPass.equals("yes")) {
+            membershipBase_Price = membershipBase_Price + 10.00;
+        }
 
         // e.) Personal trainer:
         double PT_Price = 80.00;
+        if (wantsPt.equals("yes")) {
+            membershipBase_Price = membershipBase_Price + 80.00;
+        }
 
         // f.) Gym classes:
         double gymClasses_Price = 50.00;
+        if (wantsClasses.equals("yes")) {
+            membershipBase_Price = membershipBase_Price + 50.00;
+        }
 
         // g.) If member is a student and or 25 or younger (Apply 25% discount)
        double studentDiscount = 0.00;
-       double membership_Price = 0.00;
+       double membership_discountPrice = 0.00;
 
         if (memberEnrolled.equals("yes")) {
-            studentDiscount = membership_Price * .25;
-        } else if (membersAge < 25) {
-            studentDiscount = membership_Price * .25;
+            studentDiscount = .25 * membershipBase_Price;
+
+        }if (membersAge < 25) {
+            studentDiscount = membershipBase_Price * .15;
+
+
+        // h.) Display
+        double membershipOptions = goldMembership_Price + silverMembership_Price + bronzeMembership_Price + gymClasses_Price + guestPass_Price + PT_Price;
+        double totalMembershipCost = membershipBase_Price + studentDiscount + membershipOptions;
+            System.out.println(totalMembershipCost);
 
 
         }
