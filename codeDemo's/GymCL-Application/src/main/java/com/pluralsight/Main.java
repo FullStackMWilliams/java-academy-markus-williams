ef28e3415c759fd5316439a179e8ce7a0300c8df
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-28 12:32:02 -0400

    Creating a walk/follow through for peers about Command-Line Applications

---
+++ codeDemo's/GymCL-Application/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+import java.util.Scanner;
+/*
+                                    Exercise: Gym Command-Line Application
+ Today I will build a command line application modeled after the last exercise in workbook 1.
+ Follow along and use your own variables to practice building a Command-Line Application and java fundamentals.
+ Developers instructions: Create a Command line Application that prompts/displays users about sign-up and sign-up cost.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Here I introduced a scanner called gymClerk. This will read the new members input.
+        Scanner gymClerk = new Scanner(System.in);
+
+// Step 1: Prompt the user for the following information
+
+        // a.) Whether user wants to sign-up for a gym membership or not (yes/no) include membership price per month:
+        // b.) Membership start date:
+        // c.) Member's age:
+        // d.) Whether member would like to add a guest pass:
+        // e.)
+
+    }
+}
