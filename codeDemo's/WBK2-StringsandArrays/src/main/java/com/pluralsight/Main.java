c120e03a0a93452ed18bdab8b331c44eb5ecf652
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-29 17:18:25 -0400

    Adding comments

--- codeDemo's/WBK2-StringsandArrays/src/main/java/com/pluralsight/Main.java
+++ codeDemo's/WBK2-StringsandArrays/src/main/java/com/pluralsight/Main.java
@@ -2,7 +2,14 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("=====Strings=====");
+
+        //String methods
+
+        // a.)
+        // b.)
+        // c.)
 // This method ".toUpperCase" allows the string we are calling to be printed in all caps.
         String favMovie = "Scary Movie 3".toUpperCase();
         System.out.println(favMovie);
