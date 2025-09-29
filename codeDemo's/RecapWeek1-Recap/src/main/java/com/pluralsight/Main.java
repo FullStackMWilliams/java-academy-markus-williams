ce3d6cb847dcce681e113e15b2725964ed6e652f
FullStackMWilliams <markuswilliams99@icloud.com>
2025-09-29 12:46:17 -0400

    first project you coded without references!!! ;D

---
+++ codeDemo's/RecapWeek1-Recap/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner myExam_Proctor = new Scanner(System.in);
+
+        System.out.println("Enter your test score: ");
+        int testersScore = myExam_Proctor.nextInt();
+
+        if (testersScore >= 90)
+            System.out.println("You Earned an 'A'.");
+
+        if (testersScore >= 80 && testersScore <= 89)
+            System.out.println("You earned a 'B'.");
+
+        if(testersScore >= 70 && testersScore <= 79)
+            System.out.println("You earned a 'C'.");
+
+        if (testersScore >= 60 && testersScore <=69)
+            System.out.println("You earned a 'D'.");
+
+        if (testersScore <= 59)
+            System.out.println("You earned a 'F'.");
+
+    }
+
+
+    }
+
+
+
