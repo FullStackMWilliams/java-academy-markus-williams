da4cccd80c1f379391c81c680938cde36602528b
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-01 14:13:23 -0400

    Done with HighScoreWins Exercise (ಠ︹ಠ)

--- workbook2/HighScoreWins/src/main/java/com/pluralsight/Main.java
+++ workbook2/HighScoreWins/src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,7 +15,40 @@ public class Main {
         if (gameInfo.length != 2) {
             System.out.println("Invalid format. (Please use Home:Visitors|21:9): ");
             highScore_Collector.close();
+            return;
         }
+
+        String[] teams = gameInfo[0].split(":");
+        String[] scores = gameInfo[1].split(":");
+
+        if (teams.length != 2 || scores.length != 2) {
+            System.out.println("Invalid format. PLease use format Home:Visitors|homeScore:awayScore");
+            highScore_Collector.close();
+            return;
+        }
+
+        String homeTeam = teams[0].trim();
+        String visitorTeam = teams[1].trim();
+
+        int homeScore = Integer.parseInt(scores[0].trim());
+        int visitorScore = Integer.parseInt(scores[1].trim());
+
+
+        String winner;
+        if (homeScore> visitorScore) {
+            winner = homeTeam;
+        }else if (visitorScore > homeScore) {
+            winner = visitorTeam;
+        }else {
+            winner = "Its a tie";
+        }
+
+        System.out.println("Winner: " + winner);
+        highScore_Collector.close();
+
+
+
+        
 
     }
 }
