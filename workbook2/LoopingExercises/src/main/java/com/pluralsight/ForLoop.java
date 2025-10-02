a2bbd64456a21e974379075dfa622a3404c95b68
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-02 18:09:43 -0400

    Let's get'er done boy's ୧(ಥꞈಥ)୨

---
+++ workbook2/LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("Lunch!");
+    }
+}
