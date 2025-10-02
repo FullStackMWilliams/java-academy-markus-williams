65a272b117a24b3c7baa1663544c791d3ed2949b
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-01 21:01:30 -0400

    starting first Exercise for module 2 ᕙ(⇀﹏↼‶) ᕗ

---
+++ workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class CellPhone {
+                             // Exercise: Cellphone Application
+
+
+
+
+//     Data needed to be collected:
+
+    // Serial number (ex: 100000 - 9999999)
+    // model (ex:IPhone X)
+    // PhoneNumber (ex:800-555-5555)
+    // owner (ex:Dana wyatt)
+
+// Add a parameterless constructor. Provide the following default values for all string data types in the constructor
+
+    // serialNumber = 0
+    // model = ""
+    // carrier = ""
+    // phoneNumber = ""
+    // ower = ""
+
+// Provide getter and setter methods for all 5 data members.
+
+
+
+
+
+
+
+
+
+
+}
---
+++ workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplications.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class CellPhoneApplications {
+    public static void main(String[] args) {
+
+
+    }
+}
