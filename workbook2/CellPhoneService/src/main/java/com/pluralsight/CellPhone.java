32db58fbeaa6090478cd9e6d063955af1218bf69
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-02 13:15:34 -0400

    flawless ฅʕ´•ᴥ•`ʔฅ pat yourself on the back ʕ ꈍܫꈍʔ

--- workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
+++ workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -1,35 +1,86 @@
 package com.pluralsight;
 
 public class CellPhone {
-                             // Exercise: Cellphone Application
-
-
+    // Exercise: Cellphone Application
 
 
 //     Data needed to be collected:
 
     // Serial number (ex: 100000 - 9999999)
+    private int serialNumber;
     // model (ex:IPhone X)
+    private String modelNumber;
+    // Carrier (ex: AT&T)
+    private String carrier_Provider;
     // PhoneNumber (ex:800-555-5555)
+    private String phoneNumber;
     // owner (ex:Dana wyatt)
+    private String owerFullName;
 
 // Add a parameterless constructor. Provide the following default values for all string data types in the constructor
 
-    // serialNumber = 0
-    // model = ""
-    // carrier = ""
-    // phoneNumber = ""
-    // ower = ""
+    public CellPhone() {
 
-// Provide getter and setter methods for all 5 data members.
+        // serialNumber = 0
+        this.serialNumber = 0;
 
+        // model = ""
+        this.modelNumber = "";
 
+        // carrier = ""
+        this.carrier_Provider = "";
 
+        // phoneNumber = ""
+        this.phoneNumber = "";
 
+        // ower = ""
+        this.owerFullName = "";
+    }
 
+    /*    Provide getter and setter methods for all 5 data members. */
 
+    public int getSerialNumber() {
+        return serialNumber;
+    }
 
+    public String getModelNumber() {
+        return modelNumber;
+    }
 
+    public String getCarrier_Provider() {
+        return carrier_Provider;
+    }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
+    public String getOwerFullName() {
+        return owerFullName;
+    }
+
+    /* Here will put the setters */
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public void setCarrier_Provider(String carrier_Provider) {
+        this.carrier_Provider = carrier_Provider;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setOwerFullName(String owerFullName) {
+        this.owerFullName = owerFullName;
+    }
 }
+
+
+
--- workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplications.java
+++ workbook2/CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplications.java
@@ -1,8 +1,45 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class CellPhoneApplications {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+/* This line of code uses the method we made called cell phone to collect the users input in the way we structured our cellphone
+file.
+ */
+        CellPhone phone;
+        phone = new CellPhone();
+
+        System.out.print("Enter your serial number (ex: 1000000 - 9999999): ");
+        int serialNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter your phone model (ex: IPhone X): ");
+        String model = scanner.nextLine();
+
+        System.out.print("Enter your carrier name (ex. AT&T): ");
+        String carrier = scanner.nextLine();
+
+        System.out.print("Enter your phone number (ex: 888-888-8888): ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Enter your full name (ex: John Smith): ");
+        String owerFullName = scanner.nextLine();
+
+        phone.setSerialNumber(serialNumber);
+        phone.setModelNumber(model);
+        phone.setCarrier_Provider(carrier);
+        phone.setPhoneNumber(phoneNumber);
+        phone.setOwerFullName(owerFullName);
+
+
+        System.out.println("\nPhone Details:");
+        System.out.println("Serial Number: " + phone.getSerialNumber());
+        System.out.println("Model: " +phone.getModelNumber());
+        System.out.println("Carrier: " + phone.getCarrier_Provider());
+        System.out.println("Phone Number: " + phone.getPhoneNumber());
+        System.out.println("Customer full name: " + phone.getOwerFullName());
     }
 }
