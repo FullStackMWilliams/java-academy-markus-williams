9308b4d83fd6d6bbebb8acdd788b9628c6fb6c9b
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-03 16:58:28 -0400

    〈◡︵◡〞》figured it out

--- codeDemo's/ToDoApp/src/main/java/com/pluralsight/Main.java
+++ codeDemo's/ToDoApp/src/main/java/com/pluralsight/Main.java
@@ -1,11 +1,21 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        /*  */
-        System.out.println("Welcome to your to do app.");
-        Task usersTask = new Task("Hello World");
-        System.out.println(usersTask);
+
+        Task[] tasks = new Task[3];
+
+        tasks[0] = new Task("\nMarkus world");
+        tasks[1] = new Task("\nMarko world");
+        tasks[2] = new Task("\nJosh world");
+
+        tasks[1].markCompleted();
+        for (int i = 0; i < tasks.length; i++) {
+            System.out.println(tasks[i]);
+
+
+        }
     }
 }
--- codeDemo's/ToDoApp/src/main/java/com/pluralsight/Task.java
+++ codeDemo's/ToDoApp/src/main/java/com/pluralsight/Task.java
@@ -5,12 +5,24 @@ import java.time.LocalDate;
 public class Task {
     private String title;
     private LocalDate dueDate;
-    private boolean complete;
+    private boolean completed;
     private String urgency ;
     private String notificationsMessage;
     private boolean Reminder;
 
     public Task(String _title) {
         this.title = _title;
+    }
+
+    public void markCompleted(){
+     this.completed = true;
+    }
+// Needed to add this return conditional if statement in order for the system to read our array list in our Main java class  that's why we were having trouble in having the computer tell us what we wanted
+    public String toString() {
+        if (completed) {
+            return "[Completed]" + title;
+        }else {
+            return "[Uncompleted]" + title;
+        }
     }
 }
