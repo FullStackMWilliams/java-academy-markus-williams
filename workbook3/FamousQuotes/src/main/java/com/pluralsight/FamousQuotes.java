65492f33fdaae352e098b8ab3de48038d88f6e24
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-07 13:59:56 -0400

    (≖ 。≖ ) I'm watching you

---
+++ workbook3/FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class FamousQuotes {
+    public static void main(String[] args) {
+
+        String [] quotes = {
+                "You miss 100% of the shot you don't take. - Wayne Gretzky",
+                "The only way to do great work is to love what you do. - Steve Jobs",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+
+        };
+
+        boolean continueLoop = true;
+
+        while (continueLoop) {
+
+        }
+    }
+}
