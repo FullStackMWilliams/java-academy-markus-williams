8069da9bd2611ceb0275545e82a4018af708be34
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-07 20:16:19 -0400

    (⋗ཀ ⋖″) DONE.

--- workbook3/BedtimeStories/src/main/java/com/pluralsight/Main.java
+++ workbook3/BedtimeStories/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,35 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lineNumber = 1;
+
+
+        System.out.println("Type a bed time story name out of the following 3 stories.");
+        System.out.println("1.Goldilocks.txt");
+        System.out.println("2.Hansel_and_Gretel.txt");
+        System.out.println("3.Mary_had_a_Little_Lamb");
+        String fileName = scanner.nextLine();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(lineNumber + ". " + line);
+                lineNumber++;
+            }
+
+        }catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+            System.out.println("Please make sure the file is in the same folder as the program.");
+        }
+        scanner.close();
 
     }
 }
