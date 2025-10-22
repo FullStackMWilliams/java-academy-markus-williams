3e70cfae5b92feb25bea671a6b992c277fbccc63
FullStackMWilliams <markuswilliams99@icloud.com>
2025-10-22 17:27:53 -0400

    (¬ꞈ ¬") Finished with Employee class

--- workbook4/HospitalOperations/src/main/java/com/pluralsight/Employee.java
+++ workbook4/HospitalOperations/src/main/java/com/pluralsight/Employee.java
@@ -1,4 +1,52 @@
 package com.pluralsight;
 
 public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getRegularHours() {
+        return Math.max(0, hoursWorked - 40);
+    }
+    public double getOverTimeHours() {
+        return Math.max(0, hoursWorked - 40);
+    }
+
+    public double getTotal() {
+        double regularPay = getRegularHours() * payRate;
+        double overTimePay = getOverTimeHours() * payRate;
+        return regularPay + overTimePay;
+    }
 }
